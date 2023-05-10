3b5818c553815551abd11d0458a997ecb4dd2b85
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-05-10 12:30:09 +0400

    Added execution time measurement

--- app-union-find/src/main.cpp
+++ app-union-find/src/main.cpp
@@ -1,11 +1,18 @@
+#include <chrono>
 #include <iostream>
 
-#include <include/union-find-algs/quick-find.h>
+#include "../../union-find-algs/include/weighted-quick-union.h"
 
 int main(int argc, char** argv) {
-    uf::QuickFind quick_find(3);
-    quick_find.Union(0, 1);
-    std::cout << quick_find.Count() << std::endl;
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
+    uf::WeightedQuickUnion wqu(3);
+    wqu.Union(0, 1);
+    std::cout << wqu.Count() << std::endl;
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time difference (sec) = "
+              << (std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()) /1000000.0
+              << std::endl;
     return EXIT_SUCCESS;
 }
