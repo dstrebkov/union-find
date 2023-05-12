6fb39cdc73acae1dbfcd35ebb2c0382447d63309
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-05-12 21:53:14 +0400

    Coverity measurement reports using gcovr

--- cmake/Pipeline.cmake
+++ cmake/Pipeline.cmake
@@ -1,5 +1,3 @@
-include(${CTEST_SCRIPT_DIRECTORY}/../cmake/CTestConfig.cmake)
-
 site_name(CTEST_SITE)
 set(CTEST_BUILD_NAME "${CMAKE_SYSTEM_NAME}${EXTRA_BUILD_NAME}")
 set(CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/..")
@@ -16,10 +14,19 @@ ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 
 ctest_start(Experimental)
 ctest_configure(OPTIONS "-DCMAKE_CXX_FLAGS_INIT=--coverage")
-#ctest_submit(PARTS Start Configure)
 ctest_build()
-#ctest_submit(PARTS Build)
 ctest_test()
-#ctest_submit(PARTS Test)
 ctest_coverage()
-#ctest_submit(PARTS Coverage Done)
+
+find_package(Python3 COMPONENTS Interpreter REQUIRED)
+
+set(VENV_PATH "${CTEST_BINARY_DIRECTORY}/py_env")
+set(GCOVR_EXCLUDE ".*/\(_deps|test|CMakeFiles)/")
+set(GCOVR_XML_FILE "${CTEST_BINARY_DIRECTORY}/coverage/coverage.xml")
+set(GCOVR_HTML_FILE "${CTEST_BINARY_DIRECTORY}/coverage/coverage.html")
+
+execute_process(COMMAND "${Python3_EXECUTABLE}" -m venv "${VENV_PATH}")
+execute_process(COMMAND ${VENV_PATH}/bin/python -m pip install --upgrade pip --no-warn-script-location)
+execute_process(COMMAND ${VENV_PATH}/bin/pip install gcovr==6.0)
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CTEST_BINARY_DIRECTORY}/coverage)
+execute_process(COMMAND ${VENV_PATH}/bin/gcovr --root ${CTEST_SOURCE_DIRECTORY} --exclude ${GCOVR_EXCLUDE} --xml=${GCOVR_XML_FILE} --html-details=${GCOVR_HTML_FILE})
