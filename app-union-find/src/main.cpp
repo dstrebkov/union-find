0a15e049a26dd9fa2c75a9645e27ffa3c9f02970
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-05-12 22:50:35 +0400

    Copying fixes (#8)
    * Fixing copying of public includes & libs
    
    * Fixing copying of public includes & libs (2)
    
    * app-union-find uses input from stdin

--- app-union-find/src/main.cpp
+++ app-union-find/src/main.cpp
@@ -3,21 +3,13 @@
 #include <iostream>
 #include <sstream>
 
-#include "../../union-find-algs/include/quick-find.h"
-#include "../../union-find-algs/include/quick-union.h"
-#include "../../union-find-algs/include/weighted-quick-union.h"
+#include <include/union-find-algs/quick-find.h>
+#include <include/union-find-algs/quick-union.h>
+#include <include/union-find-algs/weighted-quick-union.h>
 
 int main(int argc, char** argv) {
-    std::ifstream input_fs{"/home/dstrebkov/Devel/cpp_projects/union-find/union-find-algs/test/test-data/uf_2M.txt"};
-    if (!input_fs) {
-        std::cerr << "Input file not found." << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    std::string input(std::istreambuf_iterator<char>{input_fs}, {});
-    std::istringstream isstream{input};
     uint32_t n;
-    isstream >> n;
+    std::cin >> n;
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
@@ -26,7 +18,7 @@ int main(int argc, char** argv) {
     uf::WeightedQuickUnion weighted_quick_union(n);
 
     uf::UnionFind& uf = weighted_quick_union;
-    for (uint32_t p = 0, q = 0; isstream >> p >> q;) {
+    for (uint32_t p = 0, q = 0; std::cin >> p >> q;) {
         if (uf.Find(p) == uf.Find(q)) {
             continue;
         }
@@ -35,7 +27,7 @@ int main(int argc, char** argv) {
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Time: "
-              << (std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()) /1000000.0
+              << (std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()) / 1000000.0
               << " sec. " << std::endl;
     return EXIT_SUCCESS;
 }
