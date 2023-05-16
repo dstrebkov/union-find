6a38a25d56af184db6a986d020ffb9cbff206704
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-05-16 17:15:18 +0400

    More runners (#14)
    * app-union-find takes algorithm name from stdin
    
    * Added additional runner scripts.

--- app-union-find/src/main.cpp
+++ app-union-find/src/main.cpp
@@ -1,26 +1,37 @@
 #include <chrono>
 #include <iostream>
+#include <memory>
 
 #include <include/union-find-algs/quick-find.h>
 #include <include/union-find-algs/quick-union.h>
 #include <include/union-find-algs/weighted-quick-union.h>
 
 int main(int argc, char** argv) {
+    std::string uf_alg;
+    std::cin >> uf_alg;
+
     uint32_t n;
     std::cin >> n;
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-    uf::QuickFind quick_find(n);
-    uf::QuickUnion quick_union(n);
-    uf::WeightedQuickUnion weighted_quick_union(n);
+    std::unique_ptr<uf::UnionFind> uf;
+    if (uf_alg == "QuickFind") {
+        uf = std::make_unique<uf::QuickFind>(uf::QuickFind(n));
+    } else if (uf_alg == "QuickUnion") {
+        uf = std::make_unique<uf::QuickUnion>(uf::QuickUnion(n));
+    } else if (uf_alg == "WeightedQuickUnion") {
+        uf = std::make_unique<uf::WeightedQuickUnion>(uf::WeightedQuickUnion(n));
+    } else {
+        std::cerr << "Error: Supported values are 'QuickFind', 'QuickUnion' and 'WeightedQuickUnion'.";
+        return EXIT_FAILURE;
+    }
 
-    uf::UnionFind& uf = weighted_quick_union;
     for (uint32_t p = 0, q = 0; std::cin >> p >> q;) {
-        if (uf.Find(p) == uf.Find(q)) {
+        if (uf->Find(p) == uf->Find(q)) {
             continue;
         }
-        uf.Union(p, q);
+        uf->Union(p, q);
     }
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
--- tools/scripts/run-700K.sh
+++ tools/scripts/run-700K.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 BINARY=../../build-union-find-Desktop_Qt_6_5_0_GCC_64bit-Release/app-union-find/app-union-find
-FILE=../../union-find-algs/test/test-data/uf_2M.txt
+FILE=../../union-find-algs/test/test-data/uf_700K.txt
+
+echo "Running QuickUnion:"
+echo -e "QuickUnion\n$(cat $FILE)" > ./to_stdin.txt
+$BINARY < ./to_stdin.txt
+rm ./to_stdin.txt
 
 echo "Running WeightedQuickUnion:"
 echo -e "WeightedQuickUnion\n$(cat $FILE)" > ./to_stdin.txt
